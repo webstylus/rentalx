90effa1e2d55c08a4eec10ad92b73ffd89c37d3b
Rafael Pacífico <rafael@webstylus.com.br>
2021-11-23 22:08:18 -0300

    Add launch

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World!' })
+})
+
+app.post('/course', (request, response) => {
+    const { name } = request.body
+    return response.json({ name })
+})
+
 app.listen(3333);
