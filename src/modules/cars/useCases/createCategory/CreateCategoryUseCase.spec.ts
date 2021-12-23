5e5fa5d217eb5fa17a80516afed27f83241db13b
Rafael Pacífico <rafael@webstylus.com.br>
2021-12-23 10:59:46 -0300

    add test to categories

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,51 @@
+import { AppError } from "src/errors/AppError";
+
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategorUseCase: CreateCategoryUseCase;
+let categoiesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoiesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategorUseCase = new CreateCategoryUseCase(
+      categoiesRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const categroy = {
+      name: "Create Test",
+      description: "Category description Test",
+    };
+    await createCategorUseCase.execute({
+      name: categroy.name,
+      description: categroy.description,
+    });
+
+    const categoryCreated = await categoiesRepositoryInMemory.findByName(
+      categroy.name
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const categroy = {
+        name: "Create Test",
+        description: "Category description Test",
+      };
+      await createCategorUseCase.execute({
+        name: categroy.name,
+        description: categroy.description,
+      });
+
+      await createCategorUseCase.execute({
+        name: categroy.name,
+        description: categroy.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
